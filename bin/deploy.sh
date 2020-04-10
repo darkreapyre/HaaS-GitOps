0678f85e1737870caff719540ea06aac3e0526ad
Trenton Potgieter <darkreapyre@gmail.com>
2020-04-10 12:45:18 -0700

    Testing full deployment

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -18,14 +18,14 @@ echo ""
 aws s3 mb "s3://${bootstrap_bucket}" --region $region
 aws s3api put-bucket-versioning --bucket $bootstrap_bucket --versioning-configuration Status=Enabled --region $region
 
-# echo ""
-# echo -n "Packaging Pre-Configuration Artifacts ..."
-# echo ""
-# cd ./src/pre-config
-# zip -r ../../staging/horovod-src.zip horovod/* Dockerfile
-# aws s3 cp ../../staging/horovod-src.zip "s3://${bootstrap_bucket}/artifacts/" --acl public-read --region $region
-# aws s3 cp ./pre-config-template.yaml "s3://${bootstrap_bucket}/templates/" --acl public-read --region $region
-# cd ../../
+echo ""
+echo -n "Packaging Pre-Configuration Artifacts ..."
+echo ""
+cd ./src/pre-config
+zip -r ../../staging/horovod-src.zip horovod/* Dockerfile
+aws s3 cp ../../staging/horovod-src.zip "s3://${bootstrap_bucket}/artifacts/" --acl public-read --region $region
+aws s3 cp ./pre-config-template.yaml "s3://${bootstrap_bucket}/templates/" --acl public-read --region $region
+cd ../../
 
 echo ""
 echo -n "Packagin Webhook Artifacts ..."
@@ -39,62 +39,54 @@ aws s3 cp ./staging/webhook-template.yaml "s3://${bootstrap_bucket}/templates/" 
 # aws cloudformation package --region $region --s3-bucket $bootstrap_bucket --template ./src/front-end/front-end-template.yaml --output-template-file ./staging/front-end-template.yaml
 # aws s3 cp ./staging/front-end-template.yaml "s3://${bootstrap_bucket}/templates/" --acl public-read --region $region
 
-# echo ""
-# echo -n "Packaging MlFlow Artifacts ..."
-# echo ""
-# cd ./src/mlflow
-# zip -r ../../staging/mlflow-src.zip Dockerfile
-# aws s3 cp ../../staging/mlflow-src.zip "s3://${bootstrap_bucket}/artifacts/" --acl public-read --region $region
-# aws s3 cp ./mlflow-build-template.yaml "s3://${bootstrap_bucket}/templates/" --acl public-read --region $region
-# aws s3 cp ./mlflow-deployment-template.yaml "s3://${bootstrap_bucket}/templates/" --acl public-read --region $region
-# cd ../../
+echo ""
+echo -n "Packaging MlFlow Artifacts ..."
+echo ""
+cd ./src/mlflow
+zip -r ../../staging/mlflow-src.zip Dockerfile
+aws s3 cp ../../staging/mlflow-src.zip "s3://${bootstrap_bucket}/artifacts/" --acl public-read --region $region
+aws s3 cp ./mlflow-build-template.yaml "s3://${bootstrap_bucket}/templates/" --acl public-read --region $region
+aws s3 cp ./mlflow-deployment-template.yaml "s3://${bootstrap_bucket}/templates/" --acl public-read --region $region
+cd ../../
 
-# echo ""
-# echo -n "Packaging Build Artifacts ..."
-# echo ""
-# cd ./src/container
-# zip -r ../../staging/container-src.zip Dockerfile
-# aws s3 cp ../../staging/container-src.zip "s3://${bootstrap_bucket}/artifacts/" --acl public-read --region $region
-# aws s3 cp ../templates/*build-template.json "s3://${bootstrap_bucket}/artifacts/" --acl public-read --region $region
-# cd ../../
+echo ""
+echo -n "Packaging Build Artifacts ..."
+echo ""
+cd ./src/container
+zip -r ../../staging/container-src.zip Dockerfile
+aws s3 cp ../../staging/container-src.zip "s3://${bootstrap_bucket}/artifacts/" --acl public-read --region $region
+aws s3 cp ../templates/*build-template.json "s3://${bootstrap_bucket}/artifacts/" --acl public-read --region $region
+cd ../../
 
-# echo ""
-# echo -n "Packaging Data Pre-Processing Artifacts ..."
-# echo ""
-# cd ./src
-# zip -r ../staging/scripts.zip scripts/*
-# aws s3 cp ../staging/scripts.zip "s3://${bootstrap_bucket}/artifacts/" --acl public-read --region $region
-# cd ../
+echo ""
+echo -n "Packaging Data Pre-Processing Artifacts ..."
+echo ""
+cd ./src
+zip -r ../staging/scripts.zip scripts/*
+aws s3 cp ../staging/scripts.zip "s3://${bootstrap_bucket}/artifacts/" --acl public-read --region $region
+cd ../
 
-# echo ""
-# echo -n "Packaging SageMaker MlOps Artifacts ..."
-# echo ""
-# cd ./src/sm-mlops
-# aws cloudformation package --region $region --s3-bucket $bootstrap_bucket --template ./sm-mlops-template.yaml --output-template-file ../../staging/sm-mlops-template.yaml
-# aws s3 cp ../../staging/sm-mlops-template.yaml "s3://${bootstrap_bucket}/templates/" --ac public-read --region $region
-# cd ../../
+echo ""
+echo -n "Packaging SageMaker MlOps Artifacts ..."
+echo ""
+cd ./src/sm-mlops
+aws cloudformation package --region $region --s3-bucket $bootstrap_bucket --template ./sm-mlops-template.yaml --output-template-file ../../staging/sm-mlops-template.yaml
+aws s3 cp ../../staging/sm-mlops-template.yaml "s3://${bootstrap_bucket}/templates/" --ac public-read --region $region
+cd ../../
 
-# echo ""
-# echo -n "Packaging Post-Configuration Artifacts ..."
-# echo ""
-# cd ./src/post-config
-# aws cloudformation package --region $region --s3-bucket $bootstrap_bucket --template ./post-config-template.yaml --output-template-file ../../staging/post-config-template.yaml
-# aws s3 cp ../../staging/post-config-template.yaml "s3://${bootstrap_bucket}/templates/" --ac public-read --region $region
-# cd ../../
+echo ""
+echo -n "Packaging Post-Configuration Artifacts ..."
+echo ""
+cd ./src/post-config
+aws cloudformation package --region $region --s3-bucket $bootstrap_bucket --template ./post-config-template.yaml --output-template-file ../../staging/post-config-template.yaml
+aws s3 cp ../../staging/post-config-template.yaml "s3://${bootstrap_bucket}/templates/" --ac public-read --region $region
+cd ../../
 
 echo ""
 echo -n "Deploying CloudFormation Stack ..."
 echo ""
-# aws cloudformation deploy --stack-name $stackname --template-file ./template.yaml --capabilities CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND --parameter-overrides EmailAddress=$email S3BootstrapBucket=$bootstrap_bucket --region $region
+aws cloudformation deploy --stack-name $stackname --template-file ./deployment-template.yaml --capabilities CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND --parameter-overrides GitToken=$token EmailAddress=$email S3BootstrapBucket=$bootstrap_bucket --region $region
 
-
-########################################################################################################################################################################################################################################
-# Testing Webhook
-aws cloudformation deploy --stack-name $stackname --template-file ./deployment-template.yaml --capabilities CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND --parameter-overrides GitToken=$token S3BootstrapBucket=$bootstrap_bucket --region $region
-########################################################################################################################################################################################################################################
-
-
-# dashboard_url=$(aws cloudformation describe-stacks --stack-name $stackname --query "Stacks[0].Outputs[?OutputKey=='MlFlowURL'].OutputValue" --output text --region $region)
 cat > ./staging/experiment.json <<EOF
 {
     "Platform": "sagemaker",
